4ecef9f8ba643deec6aacdbbf5a9a42c456cd8a6
Frank Lam <frank.is.missing@gmail.com>
2020-02-20 00:38:15 -0800

    Completed item routes with passing tests

--- funds/app/controllers/items_controller.rb
+++ funds/app/controllers/items_controller.rb
@@ -1,2 +1,47 @@
 class ItemsController < ApplicationController
+  before_action :set_shop
+  before_action :set_shop_item, only:[:show, :update, :destroy]
+
+  # GET /shops/:shop_id/items
+  def index
+    json_response(@shop.items)
+  end
+
+  # GET /shops/:shop_id/items/:id
+  def show
+    json_response(@item)
+  end
+
+  # POST /shops/:shop_id/items
+  def create
+    @shop.items.create!(item_params)
+    json_response(@item, :created)
+  end
+
+  # PUT /shops/:shop_id/items/:id
+  def update
+    @item.update(item_params)
+    head :no_content
+  end
+
+  # DELETE /shops/:shop_id/items/:id
+  def destroy
+    @item.destroy
+    head :no_content
+  end
+
+  private
+
+  def item_params
+    params.permit(:name)
+  end
+
+  def set_shop
+    @shop = Shop.find(params[:shop_id])
+  end
+
+  def set_shop_item
+    @item = @shop.items.find_by!(id: params[:id]) if @shop
+  end
+
 end
---
+++ funds/spec/requests/items_spec.rb
@@ -0,0 +1,127 @@
+require 'rails_helper'
+
+RSpec.describe 'Items API' do
+  # Initialize the test data
+  let!(:shop) { create(:shop) }
+  let!(:items) { create_list(:item, 20, shop_id: shop.id) }
+  let(:shop_id) { shop.id }
+  let(:id) { items.first.id }
+
+  # Test suite for GET /shops/:shop_id/items
+  describe 'GET /shops/:shop_id/items' do
+    before { get "/shops/#{shop_id}/items" }
+
+    context 'when shop exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns all shop items' do
+        expect(json.size).to eq(20)
+      end
+    end
+
+    context 'when shop does not exist' do
+      let(:shop_id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Shop/)
+      end
+    end
+  end
+
+  # Test suite for GET /shops/:shop_id/items/:id
+  describe 'GET /shops/:shop_id/items/:id' do
+    before { get "/shops/#{shop_id}/items/#{id}" }
+
+    context 'when shop item exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the item' do
+        expect(json['id']).to eq(id)
+      end
+    end
+
+    context 'when shop item does not exist' do
+      let(:id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Item/)
+      end
+    end
+  end
+
+  # Test suite for POST /shops/:shop_id/items
+  describe 'POST /shops/:shop_id/items' do
+    let(:valid_attributes) { { name: 'Cool Sword'} }
+
+    context 'when request attributes are valid' do
+      before { post "/shops/#{shop_id}/items", params: valid_attributes }
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when an invalid request' do
+      before { post "/shops/#{shop_id}/items", params: {} }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a failure message' do
+        expect(response.body).to match(/Validation failed: Name can't be blank/)
+      end
+    end
+  end
+
+  # Test suite for PUT /shops/:shop_id/items/:id
+  describe 'PUT /shops/:shop_id/items/:id' do
+    let(:valid_attributes) { { name: 'Lightsaber' } }
+
+    before { put "/shops/#{shop_id}/items/#{id}", params: valid_attributes }
+
+    context 'when item exists' do
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+
+      it 'updates the item' do
+        updated_item = Item.find(id)
+        expect(updated_item.name).to match(/Lightsaber/)
+      end
+    end
+
+    context 'when the item does not exist' do
+      let(:id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Item/)
+      end
+    end
+  end
+
+  # Test suite for DELETE /shops/:id
+  describe 'DELETE /shops/:id' do
+    before { delete "/shops/#{shop_id}/items/#{id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
+end
