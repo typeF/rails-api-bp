bbc3eac975c8fab4191ed45f70d29160e43327eb
Frank Lam <frank.is.missing@gmail.com>
2020-02-20 18:05:54 -0800

    Added user auth with passing tests

---
+++ funds/spec/auth/authenticate_user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe AuthenticateUser do
+  # make test user
+  let(:user) { create(:user) }
+
+  # valid request object
+  subject(:valid_auth_obj) { described_class.new(user.email, user.password)}
+
+  # invalid request object
+  subject(:invalid_auth_obj) { described_class.new('fake', 'stuff')}
+
+  describe '#call' do
+    context 'when valid credentials' do
+      it 'returns an auth token' do
+        token = valid_auth_obj.call
+        expect(token).not_to be_nil
+      end
+    end
+
+    # raise Authentication Error when invalid request
+    context 'when invalid credentials' do
+      it 'raises an authentication error' do
+        expect { invalid_auth_obj.call }
+          .to raise_error(
+            ExceptionHandler::AuthenticationError,
+            /Invalid credentials/
+          )
+      end
+    end
+  end
+
+
+end
